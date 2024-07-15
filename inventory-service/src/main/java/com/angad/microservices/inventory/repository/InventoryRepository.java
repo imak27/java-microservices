ee5bb812158dcb0daa8e4cf30218ae7810512448
imak27 <adkharate5793@gmail.com>
2024-07-15 20:46:10 +0530

    inventory-service added

---
+++ inventory-service/src/main/java/com/angad/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.angad.microservices.inventory.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.angad.microservices.inventory.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> 
+{
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
